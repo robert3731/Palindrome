c4a42f32bdb7e581e9220ff70d4f46d4b951d367
Robert Chaim <robak3731@gmail.com>
2020-10-29 14:49:18 +0100

    palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,5 +1,19 @@
 def is_palindrome(palindrome):
-    if type(palindrome) == str:
-        print('ad')
+    '''
+    Function to check if argument is palindrome.
+    Argument must be string.
+    '''
 
-is_palindrome('asd')
+    if type(palindrome) == str:
+        rev = ''.join(reversed(palindrome))
+        if palindrome == rev:
+            return True
+    return False
+
+
+a = 2
+
+if is_palindrome(a):
+    print('{} is palindrome'.format(a))
+else:
+    print('{} is not palindrome'.format(a))
