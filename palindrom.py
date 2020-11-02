aaa0ce3895657c6ec88b145b4cfbf7d31254d9cf
Robert Chaim <robak3731@gmail.com>
2020-11-02 15:47:45 +0100

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,3 +1,5 @@
+#import pytest --> nie chce mi zaipmortować modułu pytest mimo że jest zainstalowany
+
 def is_palindrome(palindrome: str) -> bool:
     """
     Function to check a string is palindrome.
@@ -11,7 +13,20 @@ def is_palindrome(palindrome: str) -> bool:
     return False
 
 
-a = 'Kobyła ma mały bok!'
+def test_is_palindrome():
+    tester = 'Kobyła ma mały bok!'
+
+    result = is_palindrome(tester)
+
+    assert result is True
+
+
+#def test_raises_exception_on_non_string_arguments():
+#    with pytest.raises(TypeError):
+#        is_palindrome(1)
+
+
+a = input('Enter text you want to check:')
 
 if is_palindrome(a):
     print('{} is palindrome'.format(a))
